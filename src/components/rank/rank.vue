c6d0fd318da9b969a79d81b1b111938b5c2bb76c
KuangjuX <18630816527@163.com>
2020-10-06 23:38:43 +0800

    init

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  排名页面
+</div>
+</template>
+
+<script>
+export default {
+name: "rank"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Recommend from '../components/recommand/recommand'
+import Rank from '../components/rank/rank'
+import Singer from '../components/singer/singer'
+import Search from '../components/search/search'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/recommend',
+    component: Recommend
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/singer',
+    component: Singer
+  },
+  {
+    path: '/rank',
+    component: Rank
+  },
+  {
+    path: '/search',
+    component: Search
   }
 ]
 
